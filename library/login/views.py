aab74f4e74fbab6d0ecfb85d76e18471d45a3d25
uozou <112175474+uozou@users.noreply.github.com>
2022-09-14 16:58:17 +0530

    Add files via upload

---
+++ library/login/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import mysql.connector as sql
+nam=''
+em=''
+pwd=''
+
+# Create your views here.
+def loginaction(request):
+    global nam,em,pwd
+    if request.method=="POST":
+        m=sql.connect(host="localhost", user="root", passwd="777290", database='library')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="Name":
+                nam=value
+            if key=="Email":
+                em=value
+            if key=="Password":
+                pwd=value
+        c="insert into users('{}', '{}', '{}')".format(nam,em,pwd)
+        cursor.execute(c)
+        m.commit()
+
+    return render(request, 'signup_page.html')
